8fb19d79c7080b8090ff10aa12a60c3e6e2c8d58
unknown <terbagdasarovartur@gmail.com>
2025-02-02 15:43:09 +0400

    Add hook for searching photos

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { fetchPopularPhotos } from "../api/photoService";
+
+export const useSearch = (query: string, page: number) => {
+  const [debouncedQuery, setDebouncedQuery] = useState(query);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedQuery(query), 800);
+    return () => clearTimeout(timer);
+  }, [query]);
+
+  console.log("Searching for:", debouncedQuery, "on page:", page);
+
+  return useQuery(
+    ["photos", debouncedQuery, String(page)],
+    () => fetchPopularPhotos(debouncedQuery, page),
+    {
+      staleTime: 1000 * 60 * 5,
+      keepPreviousData: true,
+      onError: (error: Error) => console.log("Error ffetching photos:", error),
+      enabled: debouncedQuery.trim() !== "",
+      retry: 2,
+    }
+  );
+};
