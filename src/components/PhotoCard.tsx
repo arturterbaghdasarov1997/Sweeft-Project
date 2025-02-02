72db989d410edad163c2f3177466b21b0643571a
unknown <terbagdasarovartur@gmail.com>
2025-02-02 16:01:41 +0400

    Add PhotoCard component

---
+++ src/components/PhotoCard.tsx
@@ -0,0 +1,13 @@
+import { IPhotoCard } from "../interfaces/photocard.interface";
+
+export const PhotoCard: React.FC<IPhotoCard> = ({ photo, onClick }) => {
+    return (
+        <div className="photo-card" onClick={onClick}>
+            <img
+                className="photo-card-image"
+                src={photo.urls.small}
+                alt={photo.alt_description}
+            />
+        </div>
+    );
+};
