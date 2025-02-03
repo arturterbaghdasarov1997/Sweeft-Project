e70499c37578dd8c571e3c0210704383a0ba4948
unknown <terbagdasarovartur@gmail.com>
2025-02-03 19:06:18 +0400

    Add styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./NavBar.css";
+import { IoHomeSharp, IoSearch } from "react-icons/io5";
 
 interface NavBarProps {
   query?: string;
   setQuery?: (query: string) => void;
 }
 
-export const NavBar: React.FC<NavBarProps> = () => {
+export const NavBar: React.FC<NavBarProps> = ({setQuery}) => {
+    const location = useLocation();
+
+    const handleHomeClick = () => {
+        if (setQuery) {
+            setQuery("");
+        }
+    };
+
   return (
     <div className="navbar">
       <div className="navbarItem">
-        <Link to="/">Home</Link>
-        <Link to="/history">Search History</Link>
+        <Link
+            to="/"
+            onClick={handleHomeClick}
+            className={location.pathname === "/" ? "active" : ""}
+            >
+          <IoHomeSharp /> Home
+        </Link>
+        <Link
+            to="/history"
+            className={location.pathname === "/history" ? "active" : ""}
+        >
+          <IoSearch /> Search History
+        </Link>
       </div>
     </div>
   );
